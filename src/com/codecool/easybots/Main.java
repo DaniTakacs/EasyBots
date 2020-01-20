4df112fcc590d5d44b407bc6b8671624b1dc4f2a
oliverGal-05 <galoliver05@gmail.com>
2020-01-20 11:36:31 +0100

    generate map

--- src/com/codecool/easybots/Main.java
+++ src/com/codecool/easybots/Main.java
@@ -1,8 +1,47 @@
 package com.codecool.easybots;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        char[][] map = generateMap();
+        System.out.println(Arrays.deepToString(map));
+    }
+
+    public static char[][] generateMap() {
+        int rows = 30;
+        int columns = 50;
+        int lastRowElement = rows - 1;
+        int lastColumnElement = columns - 1;
+        char[][] map = new char[rows][columns];
+
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < columns; y++) {
+                if (x == 0 || x == (rows - 1)) {
+                    map[x][y] = '-';
+
+                } else {
+                    map[x][y] = ' ';
+                }
+            }
+        }
+        map[0][0] = '+';
+        map[0][lastColumnElement] = '+';
+        map[lastRowElement][0] = '+';
+        map[lastRowElement][lastColumnElement] = '+';
+        for (int x = 0; x < rows; x++) {
+            for (int y = 0; y < columns; y++) {
+                if (x > 0 && x < (rows - 1)) {
+                    map[x][0] = '|';
+                    map[x][lastColumnElement] = '|';
+                    map[lastRowElement][0] = '|';
+                    map[lastRowElement][lastColumnElement] = '|';
+
+                }
+            }
+
+        }
+        return map;
     }
 }
